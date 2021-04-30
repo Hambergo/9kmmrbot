4ac1b9eceb57f5e97f8ad9191fe58c0f8d415c77
Hambergo <ninekmmrbot@gmail.com>
2021-04-30 13:35:17 +0300

    remove steam disconnect on dota2 hellotimeout might fix crash on losing connection to dota might make bot not being able to reconnect to game coordinator

--- src/dota.ts
+++ src/dota.ts
@@ -276,7 +276,7 @@ export default class Dota {
     this.dota2.on('hellotimeout', () => {
       // this.dota2.Logger.debug = () => {};
       console.log('hello time out!');
-      this.steamClient.disconnect();
+      // this.steamClient.disconnect();
       // setTimeout(this.dota2.launch, 10000);
     });
     this.steamClient.on('loggedOff', () => {
