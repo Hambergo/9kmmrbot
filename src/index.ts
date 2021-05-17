57794974f92a9c9a90e4fdc0ced108b7b9a87ace
Hambergo <ninekmmrbot@gmail.com>
2021-05-17 22:17:17 +0300

    add uncaughtException logging to mongo

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,15 @@ import Mongo from './mongo';
 const dota = Dota.getInstance();
 const twitch = Twitch.getInstance();
 const mongo = Mongo.getInstance();
-// Twitch.api('games', { name: 'Dota 2' }).then(({ data: [{ id }] }) => console.log(id));
-// Twitch.api('streams', { game_id: 29595, first: 100 }).then(({ data: streams }) => console.log(JSON.stringify(streams.map((stream: { user_id: any; }) => stream.user_id))));
+process.on('uncaughtException', async (err) => {
+  const db = await mongo.db;
+  db.collection('errors').insertOne({
+    message: err.message,
+    name: err.name,
+    stack: err.stack,
+    createdAt: new Date(),
+  }).catch(() => {}).then(() => console.log(err));
+});
 process.on('SIGTERM', () => {
   console.log('Received SIGTERM');
   Promise.all([
