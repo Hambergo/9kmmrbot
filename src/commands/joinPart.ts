d679e56183c80d55c86d70463e9a1950a0f4d90f
Hambergo <ninekmmrbot@gmail.com>
2021-05-17 22:26:48 +0300

    added .catch() to join/part commands changed from .findOutAndUpdate() to .updateOne() on join/part commands

--- src/commands/joinPart.ts
+++ src/commands/joinPart.ts
@@ -7,14 +7,14 @@ const mongo = Mongo.getInstance();
 
 export async function join(channel: string, tags: ChatUserstate, commandName: string, ...args: string[]) {
   const db = await mongo.db;
-  twitch.join(tags.username as string);
-  db.collection('channels').findOneAndUpdate({ id: Number(tags['user-id']) }, { $set: { name: tags.username } }, { upsert: true });
+  twitch.join(tags.username as string).catch((err) => {});
+  db.collection('channels').updateOne({ id: Number(tags['user-id']) }, { $set: { name: tags.username } }, { upsert: true });
   return `Joining ${tags.username}`;
 }
 
 export async function part(channel: string, tags: ChatUserstate, commandName: string, ...args: string[]) {
   const db = await mongo.db;
-  twitch.part(tags.username as string);
-  db.collection('channels').findOneAndUpdate({ id: Number(tags['user-id']) }, { $unset: { name: '' } }, { upsert: true });
+  twitch.part(tags.username as string).catch((err) => {});
+  db.collection('channels').updateOne({ id: Number(tags['user-id']) }, { $unset: { name: '' } }, { upsert: true });
   return `Leaving ${tags.username}`;
 }
